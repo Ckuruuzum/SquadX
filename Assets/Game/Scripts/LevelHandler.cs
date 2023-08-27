dd65a8001d3ca63e6e258b2a2ce8a7c563b68bfe
Rhesus <can.kuruuzum@hotmail.com>
2023-08-27 03:02:18 +0300

    d

--- Assets/Game/Scripts/LevelHandler.cs
+++ Assets/Game/Scripts/LevelHandler.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class LevelHandler : MonoBehaviour
 {
@@ -11,7 +12,7 @@ public class LevelHandler : MonoBehaviour
     public int ally_Level = 3;
     public float ally_Experience;
     public TextMeshProUGUI ally_LevelText;
-
+    [SerializeField] private Slider ally_Exp_Slider;
     [Space]
     [Header("Enemy")]
     public int enemy_Level = 3;
@@ -19,17 +20,24 @@ public class LevelHandler : MonoBehaviour
     private int maxLevelValue = 10;
     [SerializeField] private List<int> RequiredExpToLevelUp;
 
-    
+    private void Start()
+    {
+        SetSliderMaxValue(RequiredExpToLevelUp[ally_Level - 3]);
+    }
 
     public void GainExperience(float amount, TEAM team)
     {
         if (team == TEAM.Ally)
         {
             ally_Experience = amount + ally_Experience;
+            SetSliderExp(ally_Experience);
+
             if (ally_Experience >= RequiredExpToLevelUp[ally_Level-3])
             {
                 GainLevel(TEAM.Ally);
                 ally_Experience = 0;
+                SetSliderExp(ally_Experience);
+                
             }
         }
         else if (team == TEAM.Enemy)
@@ -52,6 +60,7 @@ public class LevelHandler : MonoBehaviour
             {
                 ally_Level = maxLevelValue;
             }
+            SetSliderMaxValue(RequiredExpToLevelUp[ally_Level - 3]);
             SetCapacityText();
         }
         else if (team == TEAM.Enemy)
@@ -74,5 +83,14 @@ public class LevelHandler : MonoBehaviour
     public enum TEAM
     {
         Default = 0, Ally = 1, Enemy = 2
+    }
+
+    private void SetSliderExp(float amount)
+    {
+        ally_Exp_Slider.value = amount;
+    }
+    private void SetSliderMaxValue(float amount)
+    {
+        ally_Exp_Slider.maxValue = amount;
     }
 }
